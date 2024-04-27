d520c1c52536f0e3cf15ffe3be4e7198e125b62b
h-spear <80024307+h-spear@users.noreply.github.com>
2024-04-27 17:43:32 +0900

    add Controller Advice

---
+++ src/main/java/com/study/exception/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.study.exception.global.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.study.exception.global.exception.ApiException;
+import com.study.exception.global.response.ApiResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	// 커스텀 예외 처리
+	@ExceptionHandler(ApiException.class)
+	public ResponseEntity<ApiResponse<?>> handleCustomException(ApiException e) {
+		log.info("exception!! code={}", e.getErrorCode());
+		return ApiResponse.error(e.getErrorCode());
+	}
+}
